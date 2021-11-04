a285c6fce1aeb21eb44c341d81a8d568d0549f9e
Mohamed Tarek <MohamedTarek153@gmail.com>
2021-11-04 23:48:18 +0200

    Comp 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-//import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">الصفا و المروة للغزل</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">الصفا و المروة للغزل</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 export default App;
