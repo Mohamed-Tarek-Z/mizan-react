a166ef83a87c451276bd2121797233afa876057d
Mohamed Tarek <MohamedTarek153@gmail.com>
2021-11-13 19:56:20 +0200

    spa p1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+
+let RenderCard = ({ item, ext }) => {
+    return (
+        <Card>
+            <CardImg src={item.image + ext} alt={item.name} />
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
 
 function Home(props) {
-    return ( 
+    return (
         <div className='container'>
-            <h1> الصفا و المروة للغزل</h1>
+            <div className='row align-items-start'>
+                <div className='col-12 col-md m-1'>
+                    <RenderCard item={props.dish} ext={props.ext} />
+                </div>
+                <div className='col-12 col-md m-1'>
+                    <RenderCard item={props.promo} ext={props.ext} />
+                </div>
+                <div className='col-12 col-md m-1'>
+                    <RenderCard item={props.leader} ext={props.ext} />
+                </div>
+            </div>
         </div>
-     );
+    );
 }
 
 export default Home;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,11 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Routes, Route } from 'react-router-dom';
 
 class Main extends Component {
@@ -12,6 +16,9 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS,
             ext: 'png'
         };
     }
@@ -29,8 +36,9 @@ class Main extends Component {
             <div className="">
                 <Header />
                 <Routes>
-                    <Route path='/home' element={<Home />} />
+                    <Route path='/home' element={<Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} leader={this.state.leaders.filter((leader) => leader.featured)[0]} promo={this.state.promotions.filter((promo) => promo.featured)[0]} ext={this.state.ext} />} />
                     <Route exact path='/menu' element={<Menu dishes={this.state.dishes} ext={this.state.ext} />} />
+                    <Route path='/contactus' element={<Contact />} />
                     <Route path="*" element={<Home />} />
                 </Routes>
                 <Footer />
