f8fc78dd0223a237909fcb28d74c994cc1360d0f
Mohamed Tarek <MohamedTarek153@gmail.com>
2021-12-05 01:30:51 +0200

    Controlled forms

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,51 +1,171 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import Breadcrumb from 'react-bootstrap/breadcrumb';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-function Contact(props) {
-    return(
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <Breadcrumb.Item href="/">
-                        Home
-                    </Breadcrumb.Item>
-                    <Breadcrumb.Item active>
-                        Contact Us
-                    </Breadcrumb.Item>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>Contact Us</h3><hr />
+class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstname: '',
+            lastname: '',
+            telnum: '',
+            email: '',
+            agree: false,
+            contactType: 'Tel.',
+            message: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        let target = event.target;
+        let value = target.type === 'checkbox' ? target.checked : target.value;
+        let name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        console.log('a7a: ' + JSON.stringify(this.state));
+        alert('a7a: ' + JSON.stringify(this.state));
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <Breadcrumb.Item href="/">
+                            Home
+                        </Breadcrumb.Item>
+                        <Breadcrumb.Item active>
+                            Contact Us
+                        </Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Contact Us</h3><hr />
+                    </div>
                 </div>
-            </div>
-            <div className="row row-content">
-                <div className="col-12">
-                <h3>Location Information</h3>
-                </div>
-                <div className="col-12 col-sm-4 offset-sm-1">
+                <div className="row row-content">
+                    <div className="col-12">
+                        <h3>Location Information</h3>
+                    </div>
+                    <div className="col-12 col-sm-4 offset-sm-1">
                         <h5>Our Address</h5>
                         <address>
-                        121, Clear Water Bay Road<br />
-                        Clear Water Bay, Kowloon<br />
-                        HONG KONG<br />
-                        <i className="fa fa-phone"></i>: +852 1234 5678<br />
-                        <i className="fa fa-fax"></i>: +852 8765 4321<br />
-                        <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
+                            121, Clear Water Bay Road<br />
+                            Clear Water Bay, Kowloon<br />
+                            HONG KONG<br />
+                            <i className="fa fa-phone"></i>: +852 1234 5678<br />
+                            <i className="fa fa-fax"></i>: +852 8765 4321<br />
+                            <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
                         </address>
+                    </div>
+                    <div className="col-12 col-sm-6 offset-sm-1">
+                        <h5>Map of our Location</h5>
+                    </div>
+                    <div className="col-12 col-sm-11 offset-sm-1">
+                        <div className="btn-group" role="group">
+                            <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
+                            <a role="button" className="btn btn-info" href="https://animeidhentai.com/"><i className="fa fa-skype"></i> Skype</a>
+                            <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-12 col-sm-6 offset-sm-1">
-                    <h5>Map of our Location</h5>
-                </div>
-                <div className="col-12 col-sm-11 offset-sm-1">
-                    <div className="btn-group" role="group">
-                        <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                        <a role="button" className="btn btn-info" href="https://animeidhentai.com/"><i className="fa fa-skype"></i> Skype</a>
-                        <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+                <div className="row row-content">
+                    <div className="col-11 offset-1">
+                        <h3>Send FeedBack</h3>
+                    </div>
+                    <div className="col-10 col-md-9 offset-2">
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Group className="mb-3" as={Row} controlId='firstname'>
+                                <Col md={2}>
+                                    <Form.Label>
+                                        First Name
+                                    </Form.Label>
+                                </Col>
+                                <Col md={7}>
+                                    <Form.Control name='firstname' className="col-md-7" type="text" placeholder="First Name" value={this.state.firstname} onChange={this.handleInputChange} />
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" as={Row} controlId='lastname'>
+                                <Col md={2}>
+                                    <Form.Label>
+                                        Last Name
+                                    </Form.Label>
+                                </Col>
+                                <Col md={7}>
+                                    <Form.Control name='lastname' className="col-md-7" type="text" placeholder="Last Name" value={this.state.lastname} onChange={this.handleInputChange} />
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" as={Row} controlId='telnum'>
+                                <Col md={2}>
+                                    <Form.Label>
+                                        Contact Tel.
+                                    </Form.Label>
+                                </Col>
+                                <Col md={7}>
+                                    <Form.Control name='telnum' className="col-md-7" type="tel" placeholder="Tel. Number" value={this.state.telnum} onChange={this.handleInputChange} />
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" as={Row} controlId='email'>
+                                <Col md={2}>
+                                    <Form.Label>
+                                        Email
+                                    </Form.Label>
+                                </Col>
+                                <Col md={7}>
+                                    <Form.Control name='email' className="col-md-7" type="email" placeholder="Email" value={this.state.email} onChange={this.handleInputChange} />
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" as={Row}>
+                                <Col md={{ size: 3, offset: 2 }}>
+                                    <Form.Group>
+                                        <Form.Check name='agree' type='checkbox' label='May we Contact you?' checked={this.state.agree} onChange={this.handleInputChange} />
+                                    </Form.Group>
+                                </Col>
+                                <Col md={{ size: 2 }}>
+                                    <Form.Select name='contactType' value={this.state.contactType} onChange={this.handleInputChange}>
+                                        <option value='Tel.'>Tel.</option>
+                                        <option value='Email'>Email</option>
+                                    </Form.Select>
+                                </Col>
+                                <Col md={3}></Col>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" as={Row} controlId='feedback'>
+                                <Col md={2}>
+                                    <Form.Label>
+                                        FeedBack
+                                    </Form.Label>
+                                </Col>
+                                <Col md={7}>
+                                    <Form.Control name='message' as="textarea" placeholder="Leave a comment here" value={this.state.message} onChange={this.handleInputChange} />
+                                </Col>
+                            </Form.Group>
+                            <Form.Group className="mb-3" as={Row} controlId='Submit'>
+                                <Col md={{ size: 10, offset: 2 }}>
+                                    <Button variant="primary" type="submit">
+                                        Submit
+                                    </Button>
+                                </Col>
+                            </Form.Group>
+                        </Form>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Contact;
